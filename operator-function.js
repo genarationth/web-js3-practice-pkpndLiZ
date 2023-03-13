2cfb9f517f199f0b2b46fb788e66ad7526c0b751
pkpndLiZ <phanida.lip@gmail.com>
2023-03-13 16:30:03 +0700

    submit operation function assignment

---
+++ operator-function.js
@@ -0,0 +1,12 @@
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const a = 1, b = 2;
+
+console.log("add func result: " + add(a, b));
+console.log("substract func result: " + subtract(a, b));
+console.log("multiply func result: " + multiply(a, b));
+console.log("divide func result: " + divide(a, b));
