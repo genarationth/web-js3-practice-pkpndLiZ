5abae60c25f6f0224a4737e01bc5399beeb18c36
pkpndLiZ <phanida.lip@gmail.com>
2023-03-13 16:30:51 +0700

    submit concat function assignment

---
+++ concat-function.js
@@ -0,0 +1,5 @@
+const userInfo = (firstName, location, hobby) => {
+    console.log(`Hi, my name is ${firstName} I live in ${location} and enjoy ${hobby}`);
+};
+
+userInfo('pukki', 'bk', 'sleeping');
